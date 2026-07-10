a73eccc1356d1c949390fc34f5eeeab2ab174b08
Golden5115 <ayomide.ayoola91@gmail.com>
2026-07-10 19:01:04 +0100

    update

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,6 +1,8 @@
 import type { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
+  trustHost: true,
+  secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
   pages: {
     signIn: "/login",
   },
